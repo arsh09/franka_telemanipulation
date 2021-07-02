2e4838ade8a3e7a036a7df5975be7aaa1ce30621
Muhammad Arshad <muhammadarshad0910@gmail.com>
2021-07-02 15:26:41 +0100

    added 0.7.0 libfranka

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arsh/Documents/panda_project
-# Build directory: /home/arsh/Documents/panda_project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
