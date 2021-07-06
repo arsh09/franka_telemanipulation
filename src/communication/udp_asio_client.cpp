a603d19f760e0f4331417bfcea39577813ef5b2d
Muhammad Arshad <muhammadarshad0910@gmail.com>
2021-07-06 12:33:18 +0100

    added udp using asio instead of poco, asyn

--- src/communication/udp_asio_client.cpp
+++ src/communication/udp_asio_client.cpp
@@ -23,18 +23,22 @@ int main(int argc, char* argv[])
         udp::resolver resolver(io_service);
         udp::endpoint endpoint = *resolver.resolve({udp::v4(), argv[1], argv[2]});
 
-        std::cout << "Enter message: ";
-        char request[max_length];
-        std::cin.getline(request, max_length);
-        size_t request_length = std::strlen(request);
-        s.send_to(boost::asio::buffer(request, request_length), endpoint);
+        for (;;)
+        {
+            std::cout << "Enter message: ";
+            char request[max_length];
+            std::cin.getline(request, max_length);
+            size_t request_length = std::strlen(request);
+            s.send_to(boost::asio::buffer(request, request_length), endpoint);
 
-        char reply[max_length];
-        udp::endpoint sender_endpoint;
-        size_t reply_length = s.receive_from( boost::asio::buffer(reply, max_length), sender_endpoint);
-        std::cout << "Reply is: ";
-        std::cout.write(reply, reply_length);
-        std::cout << "\n";
+            char reply[max_length];
+            udp::endpoint sender_endpoint;
+            size_t reply_length = s.receive_from( boost::asio::buffer(reply, max_length), sender_endpoint);
+            std::cout << "Reply is: ";
+            std::cout.write(reply, reply_length);
+            std::cout << "\n";
+        }
+
     }
     
     catch (std::exception& e)
--- src/communication/udp_asio_server.cpp
+++ src/communication/udp_asio_server.cpp
@@ -6,22 +6,75 @@ using boost::asio::ip::udp;
 
 enum { max_length = 1024 };
 
-void server(boost::asio::io_service& io_service, unsigned short port)
-{
-  udp::socket sock(io_service, udp::endpoint(udp::v4(), port));
-  for (;;)
-  {
-    char data[max_length];
-    udp::endpoint sender_endpoint;
-    size_t length = sock.receive_from(
-    boost::asio::buffer(data, max_length), sender_endpoint);
-    std::cout << "Received from client: " ;
-    std::cout.write(data, length);
-    std::cout << std::endl;
+// void server(boost::asio::io_service& io_service, unsigned short port)
+// {
+//   udp::socket sock(io_service, udp::endpoint(udp::v4(), port));
+  
+//   udp::endpoint sender_endpoint;
+//   char data[max_length];
+//   sock.async_receive_from(
+//     boost::asio::buffer(data, max_length), sender_endpoint,
+//     [&data, &sock, &sender_endpoint](boost::system::error_code ec, std::size_t bytes_recvd)
+//     {
+//       if ( !ec && bytes_recvd > 0 ) 
+//       {
+//         std::cout << "Received from client (Async): " ;
+//         std::cout.write(data, bytes_recvd);
+//         std::cout << std::endl;
+//         sock.send_to(boost::asio::buffer(data, bytes_recvd), sender_endpoint);
+//       }
+//     });
 
-    sock.send_to(boost::asio::buffer(data, length), sender_endpoint);
+//   io_service.run();
+// }
+
+
+class server
+{
+public:
+  server(boost::asio::io_service& io_service, short port)
+    : socket_(io_service, udp::endpoint(udp::v4(), port))
+  {
+    do_receive();
   }
-}
+
+  void do_receive()
+  {
+    socket_.async_receive_from(
+        boost::asio::buffer(data_, max_length), sender_endpoint_,
+        [this](boost::system::error_code ec, std::size_t bytes_recvd)
+        {
+          if (!ec && bytes_recvd > 0)
+          {
+            std::cout << "Received frrom client: " ;
+            std::cout.write(data_, bytes_recvd) ;
+            std::cout << std::endl;
+            do_send(bytes_recvd);
+          }
+          else
+          {
+            do_receive();
+          }
+        });
+  }
+
+  void do_send(std::size_t length)
+  {
+    socket_.async_send_to(
+        boost::asio::buffer(data_, length), sender_endpoint_,
+        [this](boost::system::error_code /*ec*/, std::size_t /*bytes_sent*/)
+        {
+          do_receive();
+        });
+  }
+
+private:
+  udp::socket socket_;
+  udp::endpoint sender_endpoint_;
+  enum { max_length = 1024 };
+  char data_[max_length];
+};
+
 
 int main(int argc, char* argv[])
 {
@@ -34,8 +87,11 @@ int main(int argc, char* argv[])
     }
 
     boost::asio::io_service io_service;
-    server(io_service, std::atoi(argv[1]));
+    server s(io_service, std::atoi(argv[1]));
+    io_service.run();
+   
   }
+
   catch (std::exception& e)
   {
     std::cerr << "Exception: " << e.what() << "\n";
