cbdf9eadce57172f422b335085568233716c7a02
Muhammad Arshad <muhammadarshad0910@gmail.com>
2021-07-07 17:33:21 +0100

    udp

--- src/communication/udp_asio_client.cpp
+++ src/communication/udp_asio_client.cpp
@@ -28,8 +28,8 @@ enum { max_length = 8192 };
 class client 
 {
 public:
-    client(boost::asio::io_service& io_service, char* server_port, char* server_ip, char* slave_ip, int is_master)
-       : socket_( io_service, udp::endpoint(udp::v4(), std::atoi(server_port)) ),
+    client(boost::asio::io_service& io_service, short port, char* server_port, char* server_ip, char* slave_ip, int is_master)
+       : socket_( io_service, udp::endpoint(udp::v4(), port)),
        is_master(is_master)
     {
         if (is_master == 0) // master (192.168.4.5 -> Other PC hostname)
@@ -61,13 +61,22 @@ public:
             size_t count = 0;
             robot.read(  [this] (const franka::RobotState& robot_state) 
                 {   
-
-                    _master_state = robot_state;
-                    std::stringstream ss;
-                    ss << _master_state;
-                    // std::size_t sentBytes = socket_.send_to(boost::asio::buffer(ss.str()), _endpoint);
-                    do_send(ss);
-                    return true;
+                    if (is_master == 0)
+                    {
+                        _master_state = robot_state;
+                        std::stringstream ss;
+                        ss << _master_state;
+                        do_send(ss);
+                        return true;
+                    }
+                    else
+                    {
+                        _slave_state = robot_state;
+                        std::stringstream ss;
+                        ss << _slave_state;
+                        do_send(ss);
+                        return true;
+                    }
                 });
         } 
         catch (franka::Exception const& e) 
@@ -83,22 +92,30 @@ public:
         if (is_master == 0)
         {
             _endpoint = server_endpoint;
+            std::cout << "Client send starting... " << std::endl;
         }
         else
         {
             _endpoint = client_endpoint;
+            std::cout << "Server receiving starting... " << std::endl;
         }
-
         socket_.async_send_to( boost::asio::buffer(_stream.str()), _endpoint, [this](boost::system::error_code ec, std::size_t bytes_sent)
-            {
-                if (!ec && bytes_sent > 0)
+            {   
+                std::cout << "Sent bytes: " << bytes_sent << std::endl;
+                if (bytes_sent > 0)
                 {
                     if (is_master == 0)
-                    std::cout << "Sent from master to slave: " << (int) bytes_sent << std::endl;
+                    {
+                        std::cout << "Sent from master to slave: " << (int) bytes_sent << std::endl;
+                    }
                     else
-                    std::cout << "Sent from slave to master:  " << (int) bytes_sent << std::endl;
+                    {
+                        std::cout << "Sent from slave to master:  " << (int) bytes_sent << std::endl;
+                    }
                 }
-
+                
+                do_receive();                
+                
             });
     }
 
@@ -108,26 +125,31 @@ public:
         if (is_master == 0)
         {
             _endpoint = client_endpoint;
+            std::cout << "Client receiving starting... " << std::endl;
         }
         else
         {
             _endpoint = client_endpoint;
+            std::cout << "Server receiving starting... " << std::endl;
         }
         socket_.async_receive_from(
         boost::asio::buffer(receive_data_, max_length), client_endpoint,
         [this](boost::system::error_code ec, std::size_t bytes_recvd)
         {
-            if (!ec && bytes_recvd > 0)
+            std::cout << "Bytes received on socket: " << bytes_recvd << std::endl;
+            if (bytes_recvd > 0)
             {
                 if (is_master == 0)
                 {
-                    std::cout << "Received a msg from a master: " << (int) bytes_recvd << std::endl;
+                    std::cout << "Received a msg from a slave to master: " << (int) bytes_recvd << std::endl;
                 }
-                if (is_master == 1)
+                else
                 {
-                    std::cout << "Received a msg from a master: " << (int) bytes_recvd << std::endl;
-                }
-                
+                    std::cout << "Received a msg from a master to slave: " << (int) bytes_recvd << std::endl;
+                    // std::stringstream ss;
+                    // ss << _slave_state;
+                    // do_send(ss);
+                }                
                 // state_parser_json(receive_data_, _slave_state);
                 memset( receive_data_, 0, sizeof(receive_data_) );
             }
@@ -219,8 +241,10 @@ int main(int argc , char* argv[])
     }
 
     int is_master = strtol(argv[4], NULL, 10);
+    short port = 0;
+    if (is_master != 0) { port = std::atoi(argv[1]); }
     boost::asio::io_service io_service;
-    client c(io_service, argv[1], argv[2], argv[3], is_master) ;
+    client c(io_service, port, argv[1], argv[2], argv[3], is_master) ;
     io_service.run();
 }
 
