0c524e06624c0a55bd8ca5dd7e5f3c427b967414
Muhammad Arshad <arsh.hasan@hotmail.com>
2021-07-16 12:00:02 +0100

    fix logs

--- src/communication/franka_master_client.cpp
+++ src/communication/franka_master_client.cpp
@@ -99,7 +99,7 @@ public:
     {
         std::array<double, 7> q_goal = {{0, -M_PI_4, 0, -3 * M_PI_4, 0, M_PI_2, M_PI_4}};
         MotionGenerator motion_generator(0.5, q_goal);
-        std::cout << "WARNING: This example will move the robot to a pre-configured pose! "
+        std::cout << "WARNING: The robot will move to a pre-configured pose! "
                 << "Please make sure to have the user stop button at hand!" << std::endl
                 << "Press Enter to continue..." << std::endl;
         std::cin.ignore();
